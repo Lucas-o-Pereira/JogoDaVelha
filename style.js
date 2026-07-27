aedf7bf025a7e207270cf3b1e28b1f4d702e57b1
Lucas Pereira <luquinhas.lp35@hotmail.com>
2026-07-27 10:58:02 -0300

    modo claro e  escuro

--- style.js
+++ style.js
@@ -1,8 +1,17 @@
 let jogador = "X"; 
-
 function jogar(celula) {
     if (celula.innerHTML == "") {
         celula.innerHTML = jogador;
         jogador = jogador == "X" ? "O" : "X";
     }
 }
+
+let trilho = document.getElementById('trilho')
+let body = document.querySelector('body')
+let container = document.querySelector('container')
+
+ trilho.addEventListener('click', ()=>{
+        trilho.classList.toggle('dark')
+        body.classList.toggle('dark')
+        container.classList.toggle('dark')
+ })
